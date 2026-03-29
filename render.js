5b994dbd647ae9e3c0d7da57c2ebc10d37d98205
Tyce Hilton <tycehilton@Tyces-MacBook-Pro.local>
2026-03-29 15:57:04 +1100

    Move note into hero container, add arrow icon to staging button

--- render.js
+++ render.js
@@ -149,7 +149,7 @@
         if (stagingUrls.length > 1) {
           var openAllBtn = document.createElement('button');
           openAllBtn.className = 'open-all-btn';
-          openAllBtn.textContent = 'Open All Staging Links';
+          openAllBtn.innerHTML = 'Open All Staging Links <svg width="12" height="12" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin-left:4px;vertical-align:-1px;"><path d="M11 7.66667V11.6667C11 12.0203 10.8595 12.3594 10.6095 12.6095C10.3594 12.8595 10.0203 13 9.66667 13H2.33333C1.97971 13 1.64057 12.8595 1.39052 12.6095C1.14048 12.3594 1 12.0203 1 11.6667V4.33333C1 3.97971 1.14048 3.64057 1.39052 3.39052C1.64057 3.14048 1.97971 3 2.33333 3H6.33333M9 1H13M13 1V5M13 1L5.66667 8.33333" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>';
           openAllBtn.addEventListener('click', function () {
             stagingUrls.forEach(function (url) {
               window.open(url, '_blank', 'noopener');
